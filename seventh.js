a3276f68d06edc4feedb4716e91b5b95880090ec
Aswin6342 <aswinedakulathil@gmail.com>
2025-09-11 09:37:03 +0530

    new

---
+++ seventh.js
@@ -0,0 +1,16 @@
+let message = "  Coding is FUN when you practice consistently!  ";
+let len=message.length;
+console.log(len);
+let t=message.trim();
+console.log(t.length);
+console.log(t.charAt(10));
+console.log(t.charCodeAt(t.length-1));
+console.log(t.slice(10,13));
+console.log(t.substring(0,6));
+console.log(t.substr(23,8));
+let r=t.replaceAll(" ","-");
+console.log(r);
+let c=t.split(" ");
+console.log(c);
+
+
---
+++ t.js
@@ -0,0 +1,12 @@
+let message = "coding is FUN when you practice consistently";
+let arr = message.split(" ");
+let m = [];
+
+for (let i = 0; i < arr.length; i++) {
+  let str = arr[i];
+  let capitalized = str[0].toUpperCase() + str.slice(1);
+  m[i] = capitalized;
+}
+
+console.log(m.join(" ")); 
+// Output: "Coding Is Fun When You Practice Consistently"
